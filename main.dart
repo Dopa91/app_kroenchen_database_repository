6811dab87b84f0eedccfca8776a20ff28b62b0a6
Dopa91 <code.dopatka@gmail.com>
2024-09-10 15:16:51 +0200

    format code

--- main.dart
+++ main.dart
@@ -14,7 +14,8 @@ void main() {
   while (appIsRunning) {
     print("Was möchtest du als nächstes tuen?");
     print("");
-    print("Die App schließen? (B)eenden) \nDich einloggen? (E)inloggen) \noder \nNeu Registrieren? (R)egistrieren)");
+    print(
+        "Die App schließen? (B)eenden) \nDich einloggen? (E)inloggen) \noder \nNeu Registrieren? (R)egistrieren)");
 
     String? userInput = stdin.readLineSync();
 
@@ -34,58 +35,60 @@ void main() {
           bool userIsLoggedIn = true;
 
           while (userIsLoggedIn) {
-          print("Menüpunkte: Neuer Tagebucheinrag (1) oder Neuen Termin eintragen (2) Zum beenden der App (B) ");
-          String? userInputMenu = stdin.readLineSync();
-          switch (userInputMenu) {
-            case "1":
-            print("Dann wollen wir mal begeinnen, wie geht es dir heute? \nWie war dein Tag");
-            stdin.readLineSync();
-            print("Tagebucheintrag gespeichert!");
-            print("");
-            print("Was möchtest du als nächstes tuen?\nTagebucheintrag ergänzen (1) Hauptmenü (H) App beenden (B)");
-            
-            String? newUserInput = stdin.readLineSync();
-            switch (newUserInput) {
+            print(
+                "Menüpunkte: Neuer Tagebucheinrag (1) oder Neuen Termin eintragen (2) Zum beenden der App (B) ");
+            String? userInputMenu = stdin.readLineSync();
+            switch (userInputMenu) {
               case "1":
-              print("Was möchtest du noch hinzufügen, was hast du vergessen? :)");
-              stdin.readLineSync()!;
-              print("");
-              print("Tagebucheintrag gespeichert!");
-              print("");
-              userIsLoggedIn = true;
-              case "H" || "h":
-              userIsLoggedIn = true;
-              case "Beenden" || "beenden" || "b" || "B":
-              userIsLoggedIn = false;
-              appIsRunning = false;
-            }
+                print(
+                    "Dann wollen wir mal begeinnen, wie geht es dir heute? \nWie war dein Tag");
+                stdin.readLineSync();
+                print("Tagebucheintrag gespeichert!");
+                print("");
+                print(
+                    "Was möchtest du als nächstes tuen?\nTagebucheintrag ergänzen (1) Hauptmenü (H) App beenden (B)");
 
-            case "2":
-            print("Wann ist der Termin ?");
-            stdin.readLineSync();
-            print("");
-            print("Und was findest an dem Termin stat?");
-            stdin.readLineSync();
-            print("");
-            print("Alles klar!, Termin ist eingetragen!");
-            userIsLoggedIn = true;
-            
-            case "Beenden" || "beenden" || "b" || "B":
-            userIsLoggedIn = false;
-            appIsRunning = false;
-          }
+                String? newUserInput = stdin.readLineSync();
+                switch (newUserInput) {
+                  case "1":
+                    print(
+                        "Was möchtest du noch hinzufügen, was hast du vergessen? :)");
+                    stdin.readLineSync()!;
+                    print("");
+                    print("Tagebucheintrag gespeichert!");
+                    print("");
+                    userIsLoggedIn = true;
+                  case "H" || "h":
+                    userIsLoggedIn = true;
+                  case "Beenden" || "beenden" || "b" || "B":
+                    userIsLoggedIn = false;
+                    appIsRunning = false;
+                }
+
+              case "2":
+                print("Wann ist der Termin ?");
+                stdin.readLineSync();
+                print("");
+                print("Und was findest an dem Termin stat?");
+                stdin.readLineSync();
+                print("");
+                print("Alles klar!, Termin ist eingetragen!");
+                userIsLoggedIn = true;
+
+              case "Beenden" || "beenden" || "b" || "B":
+                userIsLoggedIn = false;
+                appIsRunning = false;
+            }
           }
         } else {
           print("User existiert nicht. Registreie dich bitte. :)");
-        
         }
       case "Registrieren" || "registrieren" || "r" || "R":
         print("Die Server ist momenten leider offline :(");
         appIsRunning = false;
-      default: 
-      print("Ich versteht nur Banhof...");
+      default:
+        print("Ich versteht nur Banhof...");
         appIsRunning = true;
-
     }
   }
 }
